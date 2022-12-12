8acfa9667bc9ebeb8425113d6315d39c65d5fa85
rabbani4u <rabbani.iu31@gmail.com>
2022-12-12 23:23:55 +0600

    update

--- js/script.js
+++ js/script.js
@@ -1,143 +1,130 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  $("#slides").superslides({
+    animation: "fade",
+    play: 3500,
+    pagination: false,
+  });
 
-	$('#slides').superslides({
-		animation: 'fade',
-		play: 3500,
-		pagination: false
-	});
+  var typed = new Typed(".typed", {
+    strings: ["Software Engineer.", "Web Developer.", "Student."],
+    typeSpeed: 70,
+    loop: true,
+    startDelay: 1000,
+    showCursor: false,
+  });
 
-	var typed = new Typed(".typed", {
-		strings: ["Software Engineer.", "Web Developer.", "Student."],
-		typeSpeed: 70,
-		loop: true,
-		startDelay: 1000,
-		showCursor: false
-	});
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    items: 4,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      480: {
+        items: 2,
+      },
+      768: {
+        items: 3,
+      },
+      938: {
+        items: 4,
+      },
+    },
+  });
 
-	$('.owl-carousel').owlCarousel({
-	    loop:true,
-	    items: 4,
-	    responsive:{
-	        0:{
-	            items:1
-	        },
-	        480:{
-	            items:2
-	        },
-	        768:{
-	            items:3
-	        },
-	        938:{
-	            items:4
-	        }
-	    }
-	});
+  var skillsTopOffset = $(".skillsSection").offset().top;
+  var statsTopOffset = $(".statsSection").offset().top;
+  var countUpFinished = false;
+  $(window).scroll(function () {
+    if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+      $(".chart").easyPieChart({
+        easing: "easeInOut",
+        barColor: "#fff",
+        trackColor: false,
+        scaleColor: false,
+        lineWidth: 4,
+        size: 152,
+        onStep: function (from, to, percent) {
+          $(this.el).find(".percent").text(Math.round(percent));
+        },
+      });
+    }
 
+    if (
+      !countUpFinished &&
+      window.pageYOffset > statsTopOffset - $(window).height() + 200
+    ) {
+      $(".counter").each(function () {
+        var element = $(this);
+        var endVal = parseInt(element.text());
 
-	var skillsTopOffset = $(".skillsSection").offset().top;
-	var statsTopOffset = $(".statsSection").offset().top;
-	var countUpFinished = false;
-	$(window).scroll(function() {
+        element.countup(endVal);
+      });
 
-		if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+      countUpFinished = true;
+    }
+  });
 
-			$('.chart').easyPieChart({
-		        easing: 'easeInOut',
-		        barColor: '#fff',
-		        trackColor: false,
-		        scaleColor: false,
-		        lineWidth: 4,
-		        size: 152,
-		        onStep: function(from, to, percent) {
-		        	$(this.el).find('.percent').text(Math.round(percent));
-		        }
-		    });
+  $("[data-fancybox]").fancybox();
 
+  $(".items").isotope({
+    filter: "*",
+    animationOptions: {
+      duration: 1500,
+      easing: "linear",
+      queue: false,
+    },
+  });
 
-		}
+  $("#filters a").click(function () {
+    $("#filters .current").removeClass("current");
+    $(this).addClass("current");
 
+    var selector = $(this).attr("data-filter");
 
-		if(!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
-			$(".counter").each(function() {
-				var element = $(this);
-				var endVal = parseInt(element.text());
-
-				element.countup(endVal);
-			})
-
-			countUpFinished = true;
-
-		}
-
-
-	});
-
-	$("[data-fancybox]").fancybox();
-    
     $(".items").isotope({
-		filter: '*',
-		animationOptions: {
-			duration: 1500,
-			easing: 'linear',
-			queue: false
-		}
-	});
-	
-	 $("#filters a").click(function () {
-        $("#filters .current").removeClass("current");
-        $(this).addClass("current");
+      filter: selector,
+      animationOptions: {
+        duration: 1500,
+        easing: "linear",
+        queue: false,
+      },
+    });
+    return false;
+  });
 
-        var selector = $(this).attr("data-filter");
+  const nav = $("#navigation");
+  const navTop = nav.offset().top;
 
-        $(".items").isotope({
-            filter: selector,
-            animationOptions: {
-                duration: 1500,
-                easing: 'linear',
-                queue: false
-            }
-        });
-        return false;
-    })
-    
+  $(window).on("scroll", stickyNavigation);
 
+  function stickyNavigation() {
+    var body = $("body");
 
-
-
-	const nav = $("#navigation");
-	const navTop = nav.offset().top;
-
-	$(window).on("scroll", stickyNavigation);
-
-	function stickyNavigation() {
-
-		var body = $("body");
-
-		if($(window).scrollTop() >= navTop) {
-			body.css("padding-top", nav.outerHeight() + "px");
-			body.addClass("fixedNav");
-		}
-		else {
-			body.css("padding-top", 0);
-			body.removeClass("fixedNav");
-		}
-
-	}
-
+    if ($(window).scrollTop() >= navTop) {
+      body.css("padding-top", nav.outerHeight() + "px");
+      body.addClass("fixedNav");
+    } else {
+      body.css("padding-top", 0);
+      body.removeClass("fixedNav");
+    }
+  }
 });
 
+$(document).ready(function () {
+  $(".nav-link").click(function () {
+    var linkHref = $(this).attr("href");
+    console.log($(linkHref).offset().top);
 
-$(document).ready(function(){
-    $('.nav-link').click(function(){
-        var linkHref = $(this).attr('href');
-        console.log($(linkHref).offset().top);
-        
-        
-        $('html, body').animate({
-            scrollTop: $(linkHref).offset().top
-        }, 1500);
-    })
+    $("html, body").animate(
+      {
+        scrollTop: $(linkHref).offset().top,
+      },
+      1500
+    );
+  });
 });
- 
+
 let currentYear = new Date().getFullYear();
-document.querySelector("#year").innerHTML = currentYear + "&copy;  All Right Reserved l Design By 🤍 Rabbani Khan";
+document.querySelector("#year").innerHTML =
+  currentYear + "&copy;  All Right Reserved l Design By Rabbani Khan";
